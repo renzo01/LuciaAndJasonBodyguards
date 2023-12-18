cbd80ab54dc44a60b00a35bc4a22af224a060318
renzo01 <renzogomez577@gmail.com>
2023-12-18 01:45:06 -0500

    update

--- LuciaAndJasonBodyguards.ts
+++ LuciaAndJasonBodyguards.ts
@@ -22,57 +22,55 @@ var deloreanCoords = {
     y : -863.5663452148438,
     z : -12.290136337280273
 }
-tommy.setCoordinates(shanyAutosCoords.x,shanyAutosCoords.y,shanyAutosCoords.z);
-var firstTime = true;
+tommy.setCoordinates(shanyAutosCoords.x - 20.00,shanyAutosCoords.y,shanyAutosCoords.z);
+var executed = false;
 var alreadyFollowTommy = false;
+
+var blip = Blip.AddSpriteForCoord(shanyAutosCoords.x,shanyAutosCoords.y,shanyAutosCoords.z, 35);
 var sphere = Sphere.Create(shanyAutosCoords.x,shanyAutosCoords.y,shanyAutosCoords.z,1.00);
+
+var message = ["Jason and Lucia its comming"];
+
 while (true) {
-    wait(250);
-    // if(firstTime){
-        if(firstTime && tommy.locateOnFoot2D(shanyAutosCoords.x,shanyAutosCoords.y,10.00,1.00,true)){
-            sphere.remove();
-            // var timeTravelExplosition = Fx.AddExplosion(deloreanCoords.x,deloreanCoords.y,deloreanCoords.z,1);
-            loadModel(DELORAN_ID);
-            var delorean = Car.Create(DELORAN_ID,deloreanCoords.x,deloreanCoords.y,deloreanCoords.z);
-            delorean.changeColor(1,2);
-            delorean.setProofs(false,false,true,false,false);
-            delorean.setDrivingStyle(2);
-            loadModel(JASON_ID);
-            var jason = Char.CreateInsideCar(delorean,7,73);
-            // loadModel(RUGER_ID);
-            // jason.giveWeapon(RUGER_ID,20);
-            jason.addArmor(100);
-            loadModel(LUCIA_ID);
-            var lucia = Char.CreateAsPassenger(delorean,7,24,1);
-            // loadModel(UZI_ID);
-            // lucia.giveWeapon(UZI_ID,20);
-            lucia.addArmor(100);
-            delorean.gotoCoordinates(shanyAutosCoords.x,shanyAutosCoords.y,shanyAutosCoords.z);
-            // delorean.markAsNoLongerNeeded();
-            firstTime = false;
-        }
-        if(!alreadyFollowTommy && jason.locateInCar2D(shanyAutosCoords.x,shanyAutosCoords.y,10.00,10.00, true)){
-            jason.followPlayer(tommy);
-            jason.setPersonality(10);
-            loadModel(RUGER_MODEL_ID);
-            jason.giveWeapon(RUGER_ID,20000000);
-            Streaming.MarkModelAsNoLongerNeeded(RUGER_MODEL_ID);
-            lucia.followPlayer(tommy);
-            lucia.setPersonality(10);
-            loadModel(INGRAM_MODEL_ID);
-            lucia.giveWeapon(INGRAM_ID,2000000);
-            Streaming.MarkModelAsNoLongerNeeded(INGRAM_MODEL_ID);
-            alreadyFollowTommy = true;
-        }
-        // jason.markAsNoLongerNeeded();
-        // lucia.markAsNoLongerNeeded();
-        // firstTime = false;
-    // }
+    wait(10000);
+    if(!executed && tommy.locateOnFoot2D(shanyAutosCoords.x,shanyAutosCoords.y,10.00,1.00,true)){
+        Text.PrintNow(message[0],25000,27);
+        sphere.remove();
+        blip.remove();
+        var timeTravelExplosition = Fx.AddExplosion(deloreanCoords.x,deloreanCoords.y,deloreanCoords.z,2);
+        loadModel(DELORAN_ID);
+        var delorean = Car.Create(DELORAN_ID,deloreanCoords.x,deloreanCoords.y,deloreanCoords.z);
+        delorean.changeColor(1,2);
+        delorean.setProofs(true,true,true,true,true);
+        delorean.setDrivingStyle(2);
+        loadModel(JASON_ID);
+        var jason = Char.CreateInsideCar(delorean,7,73);
+        jason.addArmor(100);
+        loadModel(LUCIA_ID);
+        var lucia = Char.CreateAsPassenger(delorean,7,24,1);
+        lucia.addArmor(100);
+        delorean.gotoCoordinates(shanyAutosCoords.x,shanyAutosCoords.y,shanyAutosCoords.z);
+        executed = true;
+        wait(300);
+    }
+    
+    if(!alreadyFollowTommy && jason.locateInCar2D(shanyAutosCoords.x,shanyAutosCoords.y,10.00,10.00, false)){
+        delorean.setProofs(false,false,false,false,false);
+        jason.followPlayer(tommy);
+        jason.setPersonality(10);
+        loadModel(RUGER_MODEL_ID);
+        jason.giveWeapon(RUGER_ID,20000000);
+        Streaming.MarkModelAsNoLongerNeeded(RUGER_MODEL_ID);
+        lucia.followPlayer(tommy);
+        lucia.setPersonality(10);
+        loadModel(INGRAM_MODEL_ID);
+        lucia.giveWeapon(INGRAM_ID,2000000);
+        Streaming.MarkModelAsNoLongerNeeded(INGRAM_MODEL_ID);
+        delorean.markAsNoLongerNeeded();
+        alreadyFollowTommy = true;
+        wait(300);
+    }
 }
-
-
-// delorean.setForwardSpeed(75.00);
-// delorean.setCruiseSpeed(90.00);
 
 function loadModel(ModelID) {
 	Streaming.RequestModel(ModelID);
